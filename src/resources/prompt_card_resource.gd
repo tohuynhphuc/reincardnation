232be2f559aaa8ab233a10784e9ba5f4e2dc82e5
tohuynhphuc <140296933+tohuynhphuc@users.noreply.github.com>
2026-08-08 22:18:50 +0700

    add test thresholds

--- src/resources/prompt_card_resource.gd
+++ src/resources/prompt_card_resource.gd
@@ -2,13 +2,25 @@ class_name PromptCardResource
 extends Resource
 
 @export var prompt_text: String = "What do you do?"
+
 @export var choice_one_text: String = "Choice A"
-@export var choice_two_text: String = "Choice B"
 @export var choice_one_consequences: Dictionary[String, int] = {
     Constants.SINS_NAME[Constants.SINS.GLUTTONY]: 0,
     Constants.SINS_NAME[Constants.SINS.LUST]: 0,
 }
+
+@export var choice_two_text: String = "Choice B"
 @export var choice_two_consequences: Dictionary[String, int] = {
     Constants.SINS_NAME[Constants.SINS.GLUTTONY]: 0,
     Constants.SINS_NAME[Constants.SINS.LUST]: 0,
+}
+
+@export var min_threshold: Dictionary[String, int] = {
+    Constants.SINS_NAME[Constants.SINS.GLUTTONY]: 0,
+    Constants.SINS_NAME[Constants.SINS.LUST]: 0,
+}
+
+@export var max_threshold: Dictionary[String, int] = {
+    Constants.SINS_NAME[Constants.SINS.GLUTTONY]: 100,
+    Constants.SINS_NAME[Constants.SINS.LUST]: 100,
 }
